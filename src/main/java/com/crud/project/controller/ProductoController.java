99f116339f821e06df9cb69c4dd0edd2ab62984b
bryam <bryamrubio312@gmail.com>
2024-09-30 19:16:56 -0500

    metodo obtener por id

--- src/main/java/com/crud/project/controller/ProductoController.java
+++ src/main/java/com/crud/project/controller/ProductoController.java
@@ -1,6 +1,7 @@
 package com.crud.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.crud.project.model.ProductoModel;
 import com.crud.project.repository.ProductoRepository;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
 public class ProductoController {
@@ -22,6 +25,16 @@ public class ProductoController {
     @GetMapping(value ="/producto")
     public Iterable<ProductoModel> getAllProductos(){
         return productoRepository.findAll();
+    }
+
+    @GetMapping("/obtener/{id}")
+    public ResponseEntity<ProductoModel> getProductById(@PathVariable("id") Integer id){
+        Optional<ProductoModel> producto = productoRepository.findById(id);
+        if (producto.isPresent()) {
+            return ResponseEntity.ok(producto.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping(value = "/producto/new")
